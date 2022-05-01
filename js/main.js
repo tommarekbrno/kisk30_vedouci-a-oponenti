0c96c50613998fa619efcb269223f8c9559d9eda
tommarekbrno <62596885+tommarekbrno@users.noreply.github.com>
2022-05-01 02:36:49 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -608,7 +608,7 @@ function showCluster(a) {
         }
         sigInst.clusters[a] = e;
         sigInst.draw(2, 2, 2, 2);
-        $GP.info_name.html("<b>" + a + "</b>");
+        $GP.info_name.html("<b>Identifikovaná komunita</b>");
         $GP.info_data.hide();
         $GP.info_p.html("Členové komunity:");
         $GP.info_link.find("ul").html(f.join(""));
